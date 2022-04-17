9b385bcaa50e57e2208530c3de4cc6c5bb4e503b
fernandomema <fernandomema@gmail.com>
2022-04-17 19:30:59 +0200

    Fix pagination error

--- modules/utils/utils.js
+++ modules/utils/utils.js
@@ -39,6 +39,6 @@ module.exports = {
 
     paginate(array, page_size, page_number) {
         // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
-        return Array.from(array).slice((page_number - 1) * page_size, page_number * page_size);
+        return array.slice((page_number - 1) * page_size, page_number * page_size);
     }
 }
