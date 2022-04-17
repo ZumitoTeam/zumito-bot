925f066c550c52997c7d5dd98d45b1f81735b772
fernandomema <fernandomema@gmail.com>
2022-04-17 19:57:16 +0200

    Remove status

--- modules/utils/apiResponses.js
+++ modules/utils/apiResponses.js
@@ -1,6 +1,5 @@
 exports.successResponse = function (res, msg) {
 	var data = {
-		status: 1,
 		message: msg
 	};
 	return res.status(200).json(data);
@@ -8,7 +7,6 @@ exports.successResponse = function (res, msg) {
 
 exports.successResponseWithData = function (res, msg, data) {
 	var resData = {
-		status: 1,
 		message: msg,
 		data: data
 	};
@@ -17,7 +15,6 @@ exports.successResponseWithData = function (res, msg, data) {
 
 exports.ErrorResponse = function (res, msg) {
 	var data = {
-		status: 0,
 		message: msg,
 	};
 	return res.status(500).json(data);
@@ -25,7 +22,6 @@ exports.ErrorResponse = function (res, msg) {
 
 exports.notFoundResponse = function (res, msg) {
 	var data = {
-		status: 0,
 		message: msg,
 	};
 	return res.status(404).json(data);
@@ -33,7 +29,6 @@ exports.notFoundResponse = function (res, msg) {
 
 exports.validationErrorWithData = function (res, msg, data) {
 	var resData = {
-		status: 0,
 		message: msg,
 		data: data
 	};
@@ -42,7 +37,6 @@ exports.validationErrorWithData = function (res, msg, data) {
 
 exports.unauthorizedResponse = function (res, msg) {
 	var data = {
-		status: 0,
 		message: msg,
 	};
 	return res.status(401).json(data);
