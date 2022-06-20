1a98681587e8cf15b5ed3e95a16c8d6f943b3914
Fernando Merino <fernandomema@gmail.com>
2022-06-20 20:38:34 +0200

    improve userinfo

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,25 +1,21 @@
 // init require
 const Discord = require('discord.js');
 const botConfig = require("@config/bot.js");
-const { default: localizify, t } = require('localizify');
-var moment = require('moment');
-const fs = require('fs');
-var { image_search } = require('duckduckgo-images-api');
-var remote = require('remote-file-size')
-const { getBotVersion, getFooter, getTulipoSettings, setTulipoSettings } = require("@modules/utils/data.js");
+const { getFooter } = require("@modules/utils/data.js");
 const emojis = require('@config/emojis.js');
 require("@modules/localization.js");
 
 // export module
 module.exports = {
 	name: "userinfo",
-	description: "Check user info",
 	aliases: ['user'],
 	ussage: 'userinfo [<user mention>]',
 	examples: ['userinfo', 'userinfo @Zumito'],
 	hidden: false,
 	admin: false,
 	nsfw: false,
+	cooldown: 0,
+	dm: true,
 	async execute(client, message, args) {
 
 		let user = message.mentions.users.first() || message.author;
@@ -38,13 +34,21 @@ module.exports = {
 
 			.setTitle('command.userinfo.title'.trans()) //emoji.info + ' ' + 
 			.setThumbnail(user.avatarURL({ dynamic: true }))
-			.setDescription('**' + 'command.userinfo.user'.trans() + ':** ' + `${user.username}` + '#' + `${user.discriminator}` + '\n**' + 'command.userinfo.nickname'.trans() + ':** ' + `${member.nickname || 'command.userinfo.no_nickname'.trans()}` + '\n**' + 'command.userinfo.id'.trans() + ':** ' + `${user.id}` + '\n**' + 'command.userinfo.color'.trans() + ':** ' + `${user.hexAccentColor}` + '\n**' + 'command.userinfo.playing'.trans() + ':** ' + game)
-			//.setImage(user.bannerURL({ dynamic: true })) // TODO: Hacerlo funcional el banner
-			.addField('command.userinfo.creation'.trans() + ': ', 'Agregar el fecha')
-			.addField('command.userinfo.login'.trans() + ': ', 'Fecha')
-			.addField('command.userinfo.roles'.trans() + ': ', `${member.roles.cache.map(m => m).join(" • ")}`) //TODO: agregar mensaje Sin roles cuando no tenga ningun rol
+			.setDescription('**' + 'command.userinfo.user'.trans() + ':** ' + `${user.username}` + '#' + `${user.discriminator}` + '\n**' + 'command.userinfo.nickname'.trans() + ':** ' + `${member.nickname || 'command.userinfo.no_nickname'.trans()}` + '\n**' + 'command.userinfo.id'.trans() + ':** ' + `${user.id}` + '\n**' + 'command.userinfo.color'.trans() + ':** ' + `${member.displayHexColor}` + '\n**' + 'command.userinfo.playing'.trans() + ':** ' + game)
+			.addField(
+				'command.userinfo.creation'.trans() + ': ', 
+				`<t:${Math.floor(member.user.createdAt / 1000)}:d>` + ' (' + `<t:${Math.floor(member.user.createdAt / 1000)}:R>` + ')')
+			.addField(
+				'command.userinfo.login'.trans() + ': ', 
+				`<t:${Math.floor(member.joinedAt / 1000)}:d>` + ' (' + `<t:${Math.floor(member.joinedAt / 1000)}:R>` + ')')
+			.addField(
+				'command.userinfo.roles'.trans() + ': ', 
+				`${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `<@&${roles.id }>`).join(" • ") || 'command.userinfo.noRoles'.trans()}`)
 			.setColor(botConfig.embeds.color)
-			.setFooter({ text: getFooter(message.member.user.tag), iconURL: message.author.avatarURL({ dynamic: true }) })
+			.setFooter({ 
+				text: getFooter((message.author || message.member.user).tag), 
+				iconURL: (message.author || message.user).avatarURL({ dynamic: true }) 
+			})
 
 		return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
 
