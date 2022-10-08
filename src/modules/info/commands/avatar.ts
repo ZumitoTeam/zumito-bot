353107dead596a6b93d7b607823ae1d574ab69c9
Fernando Merino <fernandomema@gmail.com>
2022-10-08 16:47:48 +0200

    update

--- src/modules/info/commands/avatar.ts
+++ src/modules/info/commands/avatar.ts
@@ -10,7 +10,7 @@ export class Avatar extends Command {
     examples: string[] = ['', "@zumito"]; 
     args: CommandArgDefinition[] = [{
         name: "user",
-        type: "user",
+        type: "member",
         optional: true,
     }];
     botPermissions = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'];
@@ -21,33 +21,38 @@ export class Avatar extends Command {
         let member: GuildMember = args.get('user') || (message||interaction!).member;
         if (!member) {
             (message||interaction!)?.reply({
-                content: framework.translations.get('command.avatar.error', guildSettings.lang), allowedMentions: { repliedUser: false }
+                content: framework.translations.get('command.avatar.error', guildSettings.lang), 
+                allowedMentions: { 
+                    repliedUser: false 
+                }
             });
             return;
         }
         let embed = new EmbedBuilder()
-            .setTitle( framework.translations.get('command.avatar.title', guildSettings.lang) + ' '  + `${member.user.tag}`)
+            .setTitle( framework.translations.get('command.avatar.title', guildSettings.lang, {
+                user: member.user.tag
+            }))
             .setDescription(`[Avatar URL](${member.user.displayAvatarURL({forceStatic: false, size: 4096 })})`)
             .setImage(member.user.displayAvatarURL({ 
                 forceStatic: false, 
                 size: 4096 
             }))
             .setFooter({
-                text: framework.translations.get('global.requested', guildSettings.lang) + ' ' + `${message?.author.tag || interaction?.user.tag}`,
+                text: framework.translations.get('global.requested', guildSettings.lang, {
+                    user: message?.author.tag || interaction?.user.tag
+                }),
                 iconURL: message?.author.displayAvatarURL({ forceStatic: false }) || interaction?.user.displayAvatarURL({ forceStatic: false })
             })
             .setTimestamp(new Date())
             .setColor(config.embeds.color);
-
-           
-
 
         (message||interaction!)?.reply({ 
             embeds: [embed], 
             allowedMentions: { 
                 repliedUser: false 
             } 
-        });}
+        });
+    }
 
     selectMenu({ path, interaction, client, framework }: SelectMenuParameters): void {}
 
