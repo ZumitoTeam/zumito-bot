a0083ce79f4435c9de67a2a2bfbe50264ea215f9
William Acosta <105233030+WilliamAcosta1@users.noreply.github.com>
2022-10-08 09:50:11 -0500

    Add avatar command
    * Add avatar command
    
    Co-authored-by: Fernando Merino <fernandomema@gmail.com>

---
+++ src/modules/info/commands/avatar.ts
@@ -0,0 +1,59 @@
+import { EmbedBuilder, GuildMember } from "discord.js";
+import { config } from "../../../config.js";
+import { Command, CommandArgDefinition, CommandParameters,  CommandType  } from "zumito-framework";
+import { SelectMenuParameters } from "zumito-framework/dist/types/SelectMenuParameters";
+import { type } from "os";
+
+export class Avatar extends Command {
+
+    categories = ['information'];
+    examples: string[] = ['', "@zumito"]; 
+    args: CommandArgDefinition[] = [{
+        name: "user",
+        type: "member",
+        optional: true,
+    }];
+    botPermissions = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'];
+    type = CommandType.any;
+    
+
+    execute({ message, interaction, args, client, framework, guildSettings }: CommandParameters): void {
+        let member: GuildMember = args.get('user') || (message||interaction!).member;
+        if (!member) {
+            (message||interaction!)?.reply({
+                content: framework.translations.get('command.avatar.error', guildSettings.lang), 
+                allowedMentions: { 
+                    repliedUser: false 
+                }
+            });
+            return;
+        }
+        let embed = new EmbedBuilder()
+            .setTitle( framework.translations.get('command.avatar.title', guildSettings.lang, {
+                user: member.user.tag
+            }))
+            .setDescription(`[Avatar URL](${member.user.displayAvatarURL({forceStatic: false, size: 4096 })})`)
+            .setImage(member.user.displayAvatarURL({ 
+                forceStatic: false, 
+                size: 4096 
+            }))
+            .setFooter({
+                text: framework.translations.get('global.requested', guildSettings.lang, {
+                    user: message?.author.tag || interaction?.user.tag
+                }),
+                iconURL: message?.author.displayAvatarURL({ forceStatic: false }) || interaction?.user.displayAvatarURL({ forceStatic: false })
+            })
+            .setTimestamp(new Date())
+            .setColor(config.embeds.color);
+
+        (message||interaction!)?.reply({ 
+            embeds: [embed], 
+            allowedMentions: { 
+                repliedUser: false 
+            } 
+        });
+    }
+
+    selectMenu({ path, interaction, client, framework }: SelectMenuParameters): void {}
+
+}
