1622a98bdab2ce6af3b0aacecde629171942221f
Fernando Merino <fernandomema@gmail.com>
2022-10-08 17:11:10 +0200

    Add ping command

---
+++ src/modules/info/commands/ping.ts
@@ -0,0 +1,27 @@
+import { EmbedBuilder, GuildMember } from "discord.js";
+import { config } from "../../../config.js";
+import { Command, CommandArgDefinition, CommandParameters,  CommandType  } from "zumito-framework";
+import { SelectMenuParameters } from "zumito-framework/dist/types/SelectMenuParameters";
+import { type } from "os";
+
+export class Ping extends Command {
+
+    categories = ['information'];
+    examples: string[] = ['']; 
+    args: CommandArgDefinition[] = [];
+    botPermissions = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'USE_EXTERNAL_EMOJIS'];
+    type = CommandType.any;
+
+    execute({ message, interaction, args, client, framework, guildSettings }: CommandParameters): void {
+        (message || interaction!)?.reply({
+            content: framework.translations.get('command.ping.emoji', guildSettings.lang) + ' ' + framework.translations.get('command.ping.pong', guildSettings.lang) + ' ' + `\`\`${Date.now() - (message||interaction!)?.createdTimestamp}ms\`\`` + ' | ' + framework.translations.get('command.ping.ws', guildSettings.lang) + ' ' + `\`\`${client.ws.ping}ms\`\``, 
+            allowedMentions: { 
+                repliedUser: false 
+            }
+        });
+    }
+
+    selectMenu({ path, interaction, client, framework }: SelectMenuParameters): void {
+
+    }
+}
