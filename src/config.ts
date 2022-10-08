20b98f655655f47f9411d9cc3fdcabe47a8559ee
Fernando Merino <fernandomema@gmail.com>
2022-10-08 18:05:26 +0200

    Add invite command

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 export const config: any = {
-    name: "Zumito", //Name of bot
-    supportServerURL: "https://zumito.ga/server", //Support server link
+    name: "Zumito",
+    
+    websiteURL: "https://zumito.ga/",
+    supportServerURL: "https://zumito.ga/server",
+    botInviteURL: "https://zumito.ga/server/invite",
+    botInviteImgURL: "https://raw.githubusercontent.com/fernandomema/Zumito/botinvite/assets/images/invite.png",
 
     embeds: {
         color: '#f982a2',
