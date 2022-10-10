39667744e39c876123baeee40219efcf19ca1801
Fernando Merino <fernandomema@gmail.com>
2022-10-10 14:19:49 +0200

    Add lang command

---
+++ src/modules/config/commands/lang.ts
@@ -0,0 +1,51 @@
+import { Command, CommandArgDefinition, CommandParameters, CommandType, SelectMenuParameters } from "zumito-framework";
+
+export class Lang extends Command {
+
+    categories = ['configuration'];
+    examples: string[] = ['es', 'en']; 
+    args: CommandArgDefinition[] = [{
+        name: 'lang',
+        type: 'string',
+        optional: true,
+    }];
+    botPermissions = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'USE_EXTERNAL_EMOJIS'];
+    type = CommandType.any;
+
+    async execute({ message, interaction, args, client, framework, guildSettings }: CommandParameters): Promise<void> {
+        if (args.has('lang')) {
+            const lang = args.get('lang')?.toLowerCase();
+            if (lang === 'es' || lang === 'en') {
+                guildSettings.lang = lang;
+                await guildSettings.save().catch((e: any) => console.error(e)).then((a: any) => console.log(a));
+                (message || interaction!)?.reply({
+                    content: framework.translations.get('command.lang.changed', lang, { lang }),
+                });
+            } else {
+                let text = "";
+                text += framework.translations.get('command.lang.invalid', guildSettings.lang, { lang });
+                text += '\n';
+                text += framework.translations.get('command.lang.valid', guildSettings.lang, { 
+                    langs: ['en', 'es'].join(', ') 
+                });
+
+                (message || interaction!)?.reply({
+                    content: text
+                });
+            }
+        } else {
+            (message || interaction!)?.reply({
+                content: framework.translations.get('command.lang.current', guildSettings.lang, {
+                    lang: guildSettings.lang
+                }), 
+                allowedMentions: { 
+                    repliedUser: false 
+                }
+            });
+        }
+    }
+
+    selectMenu({ path, interaction, client, framework }: SelectMenuParameters): void {
+
+    }
+}
