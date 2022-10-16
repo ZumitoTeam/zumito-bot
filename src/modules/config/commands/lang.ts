373211d8ac796bd596a88a9d05ade6aae4b333cc
William Acosta <williamacosta00814@gmail.com>
2022-10-16 18:06:04 -0500

    ✨Add lang embed

--- src/modules/config/commands/lang.ts
+++ src/modules/config/commands/lang.ts
@@ -23,28 +23,74 @@ export class Lang extends Command {
                 guildSettings.lang = lang;
                 await guildSettings.save().catch((e: any) => console.error(e)).then((a: any) => console.log(a));
                 (message || interaction!)?.reply({
-                    content: framework.translations.get('command.lang.changed', lang, { lang }),
+                    content: framework.translations.get('command.lang.changed', lang, { lang }), 
+                    allowedMentions: { 
+                            repliedUser: false 
+                        }
                 });
             } else {
-                let text = "";
-                text += framework.translations.get('command.lang.invalid', guildSettings.lang, { lang });
-                text += '\n';
-                text += framework.translations.get('command.lang.valid', guildSettings.lang, { 
-                    langs: ['en', 'es'].join(', ') 
-                });
+                let description = [
+                    framework.translations.get('command.lang.invalid', guildSettings.lang, { 
+                        lang 
+                    }),
+                    framework.translations.get('command.lang.valid', guildSettings.lang, { 
+                        langs: ['en', 'es'].join(', ') 
+                    }),
+                    framework.translations.get('command.lang.drop', guildSettings.lang)
+                    
+                ];
+
+                const invali = new EmbedBuilder()
+
+                .setTitle(framework.translations.get('command.lang.language', guildSettings.lang))
+                .setThumbnail('https://images-ext-2.discordapp.net/external/kPORDs0-YzHMbuef3WOcTuC-hRRy4noiukIFdUgqwPs/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/878950861122985996/d05ce5c0de25fd9afb4f5492f31f21fe.webp?width=609&height=609')
+                .setDescription(description.join('\n\n'))
+                .setColor(config.embeds.color);
 
                 (message || interaction!)?.reply({
-                content: text , 
-                allowedMentions: { 
+                    embeds: [invali] ,
+                    allowedMentions: { 
                         repliedUser: false 
                     }
                 });
             }
         } else {
+
+            const embed = new EmbedBuilder()
+
+                .setTitle(framework.translations.get('command.lang.language', guildSettings.lang))
+                .setThumbnail('https://images-ext-2.discordapp.net/external/kPORDs0-YzHMbuef3WOcTuC-hRRy4noiukIFdUgqwPs/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/878950861122985996/d05ce5c0de25fd9afb4f5492f31f21fe.webp?width=609&height=609')
+                .setDescription(framework.translations.get('command.lang.current', guildSettings.lang, {
+                     lang: guildSettings.lang
+                 }) + '\n\n' +
+                 framework.translations.get('command.lang.drop', guildSettings.lang)
+                 )
+                .setColor(config.embeds.color);
+
+            const row: any = new ActionRowBuilder()
+                 .addComponents(
+                    new SelectMenuBuilder()
+                        .setCustomId('select')
+                        .setPlaceholder(framework.translations.get('command.lang.select', guildSettings.lang))
+                        .addOptions(
+                            {
+                                label: 'English',
+                                description: "Set your language to English",
+                                emoji: '🇺🇸',
+                                value: 'en'
+                            },
+                            {
+                                label: 'Español',
+                                description: "Establece tu idioma a Español",
+                                emoji: '🇪🇸',
+                                value: 'es'
+                            }
+                        )
+                 );
+
             (message || interaction!)?.reply({
-                content: framework.translations.get('command.lang.current', guildSettings.lang, {
-                    lang: guildSettings.lang
-                }), 
+                embeds:[embed],
+                components: [row],
                 allowedMentions: { 
                     repliedUser: false 
                 }
