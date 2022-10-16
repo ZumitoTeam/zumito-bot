28c181c70b5fc7bbfec5a24404a0e15e9e0e647b
William Acosta <williamacosta00814@gmail.com>
2022-10-16 14:30:28 -0500

    🎃Add new text

--- src/modules/info/commands/userinfo.ts
+++ src/modules/info/commands/userinfo.ts
@@ -1,5 +1,5 @@
-import { Command, CommandArgDefinition, CommandParameters, CommandType, SelectMenuParameters } from "zumito-framework";
-import { EmbedBuilder } from "discord.js";
+import { Command, CommandArgDefinition, CommandParameters, CommandType, SelectMenuParameters, TextFormatter } from "zumito-framework";
+import { EmbedBuilder, GuildMember } from "discord.js";
 import { config } from "../../../config.js";
 import { type } from "os";
 
@@ -7,50 +7,72 @@ export class Userinfo extends Command {
 
     categories = ['information'];
     examples: string[] = ['', '<@878950861122985996>']; 
-    args: CommandArgDefinition[] = [];
+    args: CommandArgDefinition[] = [{
+        name: 'user',
+        type: 'user',
+        optional: true
+    }];
     botPermissions = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'USE_EXTERNAL_EMOJIS'];
     type = CommandType.any;
     
     execute({ message, interaction, args, client, framework, guildSettings }: CommandParameters): void {
 
-        let text = framework.translations.get('command.userinfo.user', guildSettings.lang, {
-            name: "William Acosta#0001"
-        })
+        let user = args.get('user') || (message||(interaction!)).member!.user;
+        let member: GuildMember = (message||(interaction!)).guild?.members.cache.get(user.id) as GuildMember;
 
-        let text0 = framework.translations.get('command.userinfo.nickname', guildSettings.lang, {
-            nick: framework.translations.get('command.userinfo.noNickname', guildSettings.lang)
-        })
+        let userCreateDate = user.createdAt;
+        let userGuildJoinDate = member.joinedAt as Date;
 
-        let text1 = framework.translations.get('command.userinfo.id', guildSettings.lang, {
-            id: "57547544564564"
-        })
+        let userRoles: any = member.roles.cache;
+        if (userRoles.size > 10) {
+            userRoles = framework.translations.get('command.userinfo.roles', guildSettings.language, {
+                roles: userRoles.size
+            });
+        } else if (userRoles <= 1) { // If the user has no roles or only @everyone
+            userRoles = framework.translations.get('command.userinfo.noRoles', guildSettings.language);
+        } else {
+            // Get the roles without @everyone
+            userRoles = userRoles.filter((r: any) => r.id !== (message||(interaction!)).guild?.id)
+                .map((r: any) => r)
+                .join(' • ');
+        }
 
-        let text2 = framework.translations.get('command.userinfo.color', guildSettings.lang, {
-            color: "#45656"
-        })
+        let statusGame = member?.presence?.activities[0];
 
-        let text3 = framework.translations.get('command.userinfo.playing', guildSettings.lang, {
-            playing: framework.translations.get('command.userinfo.noPlaying', guildSettings.lang)
-        })
+        let description = [
+            framework.translations.get('command.userinfo.user', guildSettings.lang, {
+                name: user.tag, 
+            }),
+            framework.translations.get('command.userinfo.nickname', guildSettings.lang, {
+                nick: (member.nickname || framework.translations.get('command.userinfo.noNickname', guildSettings.lang))
+            }),
+            framework.translations.get('command.userinfo.id', guildSettings.lang, {
+                id: user.id
+            }),
+            framework.translations.get('command.userinfo.playing', guildSettings.lang, {
+                playing: statusGame || framework.translations.get('command.userinfo.noPlaying', guildSettings.lang)
+            })
+        ];
 
         const embed = new EmbedBuilder()
             .setTitle(framework.translations.get('command.userinfo.title', guildSettings.lang))
-            .setThumbnail('https://i.pinimg.com/originals/d9/56/9b/d9569bbed4393e2ceb1af7ba64fdf86a.jpg')
-            .setDescription(
-                text + "\n" + 
-                text0 + "\n" + 
-                text1 + "\n" +
-                text2 + "\n" +
-                text3 + "\n"
-                )
+            .setThumbnail(member.displayAvatarURL({ size: 4096, forceStatic: false }))
+            .setDescription(description.join('\n'))
             .addFields(
-                { name: framework.translations.get('command.userinfo.creation', guildSettings.lang), value: 'nddndn' },
-                { name: framework.translations.get('command.userinfo.login', guildSettings.lang), value: "dkdkdk" },
-                { name: framework.translations.get('command.userinfo.roles', guildSettings.lang), value: framework.translations.get('command.userinfo.noRoles', guildSettings.lang) }
+                { 
+                    name: framework.translations.get('command.userinfo.creation', guildSettings.lang), 
+                    value: TextFormatter.getTimestampFromDate(userCreateDate, 'd') + ` (${TextFormatter.getTimestampFromDate(userCreateDate, 'R')})` // '16/12/2017 (hace 1 año)' 
+                }, { 
+                    name: framework.translations.get('command.userinfo.login', guildSettings.lang), 
+                    value: TextFormatter.getTimestampFromDate(userGuildJoinDate, 'd') + ` (${TextFormatter.getTimestampFromDate(userGuildJoinDate, 'R')})` // '16/12/2017 (hace 1 año)' 
+                }, { 
+                    name: framework.translations.get('command.userinfo.roles', guildSettings.lang), 
+                    value: userRoles || 'Error getting roles'
+                }
             )    
             .setFooter({
                 text: framework.translations.get('global.requested', guildSettings.lang, {
-                    user: message?.author.tag || interaction?.user.tag
+                        user: message?.author.tag || interaction?.user.tag
                     }),
                 iconURL: message?.author.displayAvatarURL({ forceStatic: false }) || interaction?.user.displayAvatarURL({ forceStatic: false })
             })
