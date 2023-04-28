054cdb8579755afb9db87212119493459fd075c0
fernandomema <fernandomema@gmail.com>
2023-04-28 20:53:54 +0200

    rebase changes

--- src/config/Global.ts
+++ src/config/Global.ts
@@ -3,5 +3,8 @@ export const global: any = {
     supportServerURL: "https://zumito.ga/server",
     embeds: {
         color: '#f982a2',
-    }
+    },
+    websiteURL: "https://zumito.ga/",
+    botInviteURL: "https://zumito.ga/server/invite",
+    botInviteImgURL: "https://raw.githubusercontent.com/fernandomema/Zumito/botinvite/assets/images/invite.png",
 }
