2fa66f1c0024b82ff413508d8ef8b0f0366e3673
William Acosta <105233030+WilliamAcosta1@users.noreply.github.com>
2023-06-23 11:15:30 -0500

    Update Links.ts

--- src/config/Links.ts
+++ src/config/Links.ts
@@ -5,6 +5,6 @@ export const links: any = {
       sites: {
             support: "https://bot.zumito.dev/server",
             website: "https://bot.zumito.dev/",
-            invite: "https://bot.zumito.dev/server/invite"
+            invite: "https://bot.zumito.dev/invite"
       }
 }
