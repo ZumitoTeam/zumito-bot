052d425ebc8932d5947cdf527a6e3aae4c2381e0
WilliamAcosta1 <williamacosta00814@gmail.com>
2024-01-27 17:08:57 -0500

    Update userinfo

--- src/modules/info/commands/userinfo.ts
+++ src/modules/info/commands/userinfo.ts
@@ -1,11 +1,11 @@
-import { Command, CommandArgDefinition, CommandParameters, CommandType, SelectMenuParameters, TextFormatter } from "zumito-framework";
+import { EmbedBuilder } from "discord.js";
+import { Command, CommandArgDefinition, CommandParameters, CommandType, SelectMenuParameters, TextFormatter, EmojiFallback } from "zumito-framework";
 import { config } from "../../../config/index.js";
-import { EmbedBuilder } from "zumito-framework/discord";
 
 export class UserInfo extends Command {
 
     categories = ['information'];
-    examples: string[] = ['', '@Zumito']; 
+    examples: string[] = ['', '@zumito']; 
     args: CommandArgDefinition[] = [{
         name: 'user',
         type: 'user',
@@ -18,35 +18,56 @@ export class UserInfo extends Command {
 
         let user = args.get('user') || (message||(interaction!)).member!.user;
         let member: any = (message||(interaction!)).guild?.members.cache.get(user.id);
-
         let userCreateDate = user.createdAt;
         let userGuildJoinDate = member.joinedAt as Date;
+        const isOwner = member?.guild && member.guild.ownerId === user?.id;
+        const ownerEmoji = isOwner ? '👑' : '';
+        const finalName = `${ownerEmoji}`+ ' ' +`${isOwner ? user.globalName : user.displayName}`;
+        
+        let badges = user.flags.toArray();
+        const badgeEmojiMap = {
+            HypeSquadOnlineHouse1: EmojiFallback.getEmoji(client, '1200907027896086598', '🛡️'),
+            HypeSquadOnlineHouse2: EmojiFallback.getEmoji(client, '1200907027896086598', '🛡️'),
+            HypeSquadOnlineHouse3: EmojiFallback.getEmoji(client, '1200910182914465842', '🛡️'),
+            ActiveDeveloper: EmojiFallback.getEmoji(client, '1200907904543371284', '💻')
+          };
+        let badgesWithEmojis = badges.map((badge: string | number) => badgeEmojiMap[badge as keyof typeof badgeEmojiMap]);
+
 
         let userRoles: any = member.roles.cache;
         if (userRoles.size > 10) {
             userRoles = framework.translations.get('command.userinfo.roles', guildSettings.language, {
                 roles: userRoles.size
             });
-        } else if (userRoles <= 1) { // If the user has no roles or only @everyone
+        } else if (userRoles <= 1) {
             userRoles = framework.translations.get('command.userinfo.noRoles', guildSettings.language);
         } else {
-            // Get the roles without @everyone
             userRoles = userRoles.filter((r: any) => r.id !== (message||(interaction!)).guild?.id)
                 .map((r: any) => r)
                 .join(' • ');
         }
 
         let statusGame = member?.presence?.activities[0];
-
+       
         let description = [
+            framework.translations.get('command.userinfo.info', guildSettings.lang),
+            framework.translations.get('command.userinfo.id', guildSettings.lang, {
+                id: user.id
+            }),
             framework.translations.get('command.userinfo.user', guildSettings.lang, {
-                name: user.tag, 
+                user: user.username, 
+            }),
+            framework.translations.get('command.userinfo.name', guildSettings.lang, {
+                name: user.globalName, 
             }),
             framework.translations.get('command.userinfo.nickname', guildSettings.lang, {
                 nick: (member.nickname || framework.translations.get('command.userinfo.noNickname', guildSettings.lang))
             }),
-            framework.translations.get('command.userinfo.id', guildSettings.lang, {
-                id: user.id
+            framework.translations.get('command.userinfo.color', guildSettings.lang, {
+                color: user.color
+            }),
+            framework.translations.get('command.userinfo.badges', guildSettings.lang, {
+                badges: badgesWithEmojis.join(' ')
             }),
             framework.translations.get('command.userinfo.playing', guildSettings.lang, {
                 playing: statusGame || framework.translations.get('command.userinfo.noPlaying', guildSettings.lang)
@@ -54,31 +75,34 @@ export class UserInfo extends Command {
         ];
 
         const embed = new EmbedBuilder()
-            .setTitle(framework.translations.get('command.userinfo.title', guildSettings.lang))
+            .setTitle(finalName)
+            .setURL('https://discord.com/users/' + user.id)
             .setThumbnail(member.displayAvatarURL({ size: 4096, forceStatic: false }))
             .setDescription(description.join('\n'))
             .addFields(
                 { 
-                    name: framework.translations.get('command.userinfo.creation', guildSettings.lang), 
-                    value: TextFormatter.getTimestampFromDate(userCreateDate, 'd') + ` (${TextFormatter.getTimestampFromDate(userCreateDate, 'R')})` // '16/12/2017 (hace 1 año)' 
+                    name: framework.translations.get('command.userinfo.membership', guildSettings.lang), 
+                    value: TextFormatter.getTimestampFromDate(userCreateDate, 'f') + ` (${TextFormatter.getTimestampFromDate(userCreateDate, 'R')})`
                 }, { 
-                    name: framework.translations.get('command.userinfo.login', guildSettings.lang), 
-                    value: TextFormatter.getTimestampFromDate(userGuildJoinDate, 'd') + ` (${TextFormatter.getTimestampFromDate(userGuildJoinDate, 'R')})` // '16/12/2017 (hace 1 año)' 
+                    name: framework.translations.get('command.userinfo.join', guildSettings.lang, {
+                        server: (message?.guild?.name || interaction!.guild!.name)
+                    }), 
+                    value: TextFormatter.getTimestampFromDate(userGuildJoinDate, 'f') + ` (${TextFormatter.getTimestampFromDate(userGuildJoinDate, 'R')})`
                 }, { 
                     name: framework.translations.get('command.userinfo.roles', guildSettings.lang), 
-                    value: userRoles || 'Error getting roles'
+                    value: userRoles || framework.translations.get('command.userinfo.noRoles', guildSettings.language)
                 }
             )    
             .setFooter({
                 text: framework.translations.get('global.requested', guildSettings.lang, {
-                        user: message?.author.tag || interaction?.user.tag
+                        user: message?.author.globalName || interaction?.user.globalName
                     }),
                 iconURL: message?.author.displayAvatarURL({ forceStatic: false }) || interaction?.user.displayAvatarURL({ forceStatic: false })
             })
             .setColor(config.global.embeds.color);
 
-        (message || interaction!)?.reply({
-            embeds: [embed],
+        (message || interaction!)?.reply({ 
+            embeds: [embed], 
             allowedMentions: { 
                 repliedUser: false 
             }
