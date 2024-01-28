dcccbfd07838ed470461c31c5a99f78424e7d11b
WilliamAcosta1 <williamacosta00814@gmail.com>
2024-01-28 10:58:27 -0500

    Improved translation

--- src/modules/info/commands/avatar.ts
+++ src/modules/info/commands/avatar.ts
@@ -1,6 +1,5 @@
-import { EmbedBuilder, GuildMember } from "discord.js";
-import { Command, CommandArgDefinition, CommandParameters,  CommandType  } from "zumito-framework";
-import { SelectMenuParameters } from "zumito-framework";
+import { EmbedBuilder, GuildMember, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import { Command, CommandArgDefinition, CommandParameters, CommandType, SelectMenuParameters } from "zumito-framework";
 import { config } from "../../../config/index.js";
 
 export class Avatar extends Command {
@@ -29,23 +28,25 @@ export class Avatar extends Command {
         }
         let embed = new EmbedBuilder()
             .setTitle( framework.translations.get('command.avatar.title', guildSettings.lang, {
-                user: member.user.tag
+                user: member.user.globalName || member.user.displayName
             }))
-            .setDescription(`[Avatar URL](${member.user.displayAvatarURL({forceStatic: false, size: 4096 })})`)
             .setImage(member.user.displayAvatarURL({ 
                 forceStatic: false, 
                 size: 4096 
             }))
-            .setFooter({
-                text: framework.translations.get('global.requested', guildSettings.lang, {
-                    user: message?.author.tag || interaction?.user.tag
-                }),
-                iconURL: message?.author.displayAvatarURL({ forceStatic: false }) || interaction?.user.displayAvatarURL({ forceStatic: false })
-            })
             .setColor(config.global.embeds.color);
+
+            const row: any = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                .setLabel(framework.translations.get('command.avatar.button.browser', guildSettings.lang))
+                .setStyle(ButtonStyle.Link)
+                .setURL(`${member.user.displayAvatarURL({forceStatic: false, size: 4096 })}`)
+            );
 
         (message||interaction!)?.reply({ 
             embeds: [embed], 
+            components: [row],
             allowedMentions: { 
                 repliedUser: false 
             } 
