bd08e6d8c6e0211e0642e01846fe1e776bb9e039
WilliamAcosta1 <williamacosta00814@gmail.com>
2024-01-30 17:55:45 -0500

    Refactor

--- src/config/Links.ts
+++ src/config/Links.ts
@@ -1,7 +1,7 @@
 export const links: any = {
-      sites: {
+      
             support: "https://bot.zumito.dev/server",
             website: "https://bot.zumito.dev/",
             invite: "https://bot.zumito.dev/invite"
-      }
+
 }
