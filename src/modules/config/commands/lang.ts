9e3b3aae0df5e7104e826fbe260cd283cc48d303
WilliamAcosta1 <williamacosta00814@gmail.com>
2024-08-21 14:33:56 -0500

    Update src/modules/config/commands/lang.ts

--- src/modules/config/commands/lang.ts
+++ src/modules/config/commands/lang.ts
@@ -25,12 +25,12 @@ export class Lang extends Command {
 
     private formatLang(lang: string): string {
         switch (lang) {
-            case 'en':
-                return ':flag_us: English';
-            case 'es':
-                return ':flag_es: Español';
-            default:
-                return lang;
+              case 'en':
+                  return ':flag_us: English';
+              case 'es':
+                  return ':flag_es: Español';
+              default:
+                  return lang;
         }
     }
 
