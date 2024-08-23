c0ee7cf8c7b338e45882c6d846482c77690dd67d
fernandomema <fernandomema@gmail.com>
2024-08-23 15:11:24 +0200

    Fix eslint pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,25 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default [
-    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
     {
-        plugins: {
+        "plugins": {
             "@typescript-eslint": typescriptEslint,
         },
 
-        languageOptions: {
-            parser: tsParser,
-            globals: {
+        "languageOptions": {
+            "parser": tsParser,
+            "globals": {
                 "node": true,
             }
         },
 
-        rules: {
+        "files": [
+            '**/*.js',
+            '**/*.ts',
+        ],
+
+        "rules": {
             "no-unused-vars": "error",
             "no-undef": "error",
             "indent": ["error", 4],
