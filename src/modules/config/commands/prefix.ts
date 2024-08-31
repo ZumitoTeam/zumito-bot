f863fec76ac194ed400df069e6c158390e5f3a38
WilliamAcosta1 <williamacosta00814@gmail.com>
2024-08-30 20:58:22 -0500

    New command

---
+++ src/modules/config/commands/prefix.ts
@@ -0,0 +1,44 @@
+import { Command, CommandParameters, CommandType, SelectMenuParameters, EmojiFallback, ServiceContainer } from "zumito-framework";
+
+export class Prefix extends Command {
+
+    categories = ['information'];
+    examples: string[] = ['', '!', 'h!']; 
+    args = [{
+        name: "prefix",
+        type: "string",
+        optional: true,
+    }];
+    botPermissions = ['VIEW_CHANNEL', 'SEND_MESSAGES', 'USE_EXTERNAL_EMOJIS'];
+    type = CommandType.any;
+    adminOnly = true;
+
+    emojiFallback: EmojiFallback; 
+
+    constructor() {
+        super();
+        this.emojiFallback = ServiceContainer.getService(EmojiFallback) as EmojiFallback;
+    }
+
+    execute({ message, interaction, trans }: CommandParameters): void {
+
+        /* Layou messages
+            `${this.emojiFallback.getEmoji('', 'ℹ️')  } ${  trans('message', { prefix: ['h!'] })}`,
+            `${this.emojiFallback.getEmoji('', '✅')  } ${ trans('changed', { prefix: ['h-'] })}`,
+            `${this.emojiFallback.getEmoji('', '🚫')  } ${ trans('permissions', { permissions: ['Administrator'] })}`,
+            `${this.emojiFallback.getEmoji('', '⚠️')  } ${ trans('directMSG')}`,
+            */   
+
+        (message || interaction!)?.reply({
+            content: `${this.emojiFallback.getEmoji('', '⚠️')  } ${ trans('directMSG')}`, 
+            allowedMentions: { 
+                repliedUser: false 
+            },
+            ephemeral:true
+        });
+    }
+
+    selectMenu({  }: SelectMenuParameters): void {
+
+    }
+}
