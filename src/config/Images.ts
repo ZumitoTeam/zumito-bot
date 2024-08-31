3d403093dda69d1450ea800b60bddf05e52572c7
WilliamAcosta1 <williamacosta00814@gmail.com>
2024-08-31 18:06:11 -0500

    Image storage file added

---
+++ src/config/Images.ts
@@ -0,0 +1,4 @@
+export const images: any = {
+    
+    unboxing: 'https://media.discordapp.net/attachments/989703145686982666/1279515679753633822/unboxing_j3qp3kgjq8au_512.png?ex=66d4b956&is=66d367d6&hm=bbb33d5a5aa1ce018034f8178400153d93bcb5989557eecc76068ee3c7e86d23&=&format=webp&quality=lossless',
+}
