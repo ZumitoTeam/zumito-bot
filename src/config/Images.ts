cb202a036e003fa1c87c04d97c92e5a788442125
WilliamAcosta1 <williamacosta00814@gmail.com>
2025-01-06 20:04:30 -0500

    "chore:Add banana modal"

--- src/config/Images.ts
+++ src/config/Images.ts
@@ -1,4 +1,5 @@
 export const images: any = {
     
     unboxing: 'https://media.discordapp.net/attachments/989703145686982666/1279515679753633822/unboxing_j3qp3kgjq8au_512.png?ex=66d4b956&is=66d367d6&hm=bbb33d5a5aa1ce018034f8178400153d93bcb5989557eecc76068ee3c7e86d23&=&format=webp&quality=lossless',
+    banana: 'https://media.discordapp.net/attachments/989703145686982666/1325987247140634688/banana.png?ex=677dc95b&is=677c77db&hm=b1b3c99faf58172d22e3f430f51e4269a17d33d5e59dc7c5c1b5129ffa805a66&=&format=webp&quality=lossless'
 }
