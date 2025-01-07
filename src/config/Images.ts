b7fd911a20b9e15f00babd692e91a0dc11391924
WilliamAcosta1 <williamacosta00814@gmail.com>
2025-01-06 19:05:39 -0500

    "chore:Add 8ball modal"

--- src/config/Images.ts
+++ src/config/Images.ts
@@ -1,5 +1,6 @@
 export const images: any = {
     
     unboxing: 'https://media.discordapp.net/attachments/989703145686982666/1279515679753633822/unboxing_j3qp3kgjq8au_512.png?ex=66d4b956&is=66d367d6&hm=bbb33d5a5aa1ce018034f8178400153d93bcb5989557eecc76068ee3c7e86d23&=&format=webp&quality=lossless',
-    banana: 'https://media.discordapp.net/attachments/989703145686982666/1325987247140634688/banana.png?ex=677dc95b&is=677c77db&hm=b1b3c99faf58172d22e3f430f51e4269a17d33d5e59dc7c5c1b5129ffa805a66&=&format=webp&quality=lossless'
+    banana: 'https://media.discordapp.net/attachments/989703145686982666/1325987247140634688/banana.png?ex=677dc95b&is=677c77db&hm=b1b3c99faf58172d22e3f430f51e4269a17d33d5e59dc7c5c1b5129ffa805a66&=&format=webp&quality=lossless',
+    eightBall: 'https://media.discordapp.net/attachments/989703145686982666/1325974685359542372/8ball.png?ex=677dbda8&is=677c6c28&hm=ed7b1ef10735a4bf077cf8b72a74ba4ca143fe04cbd26cf6680f744262e28918&=&format=webp&quality=lossless',
 }
---
+++ src/modules/fun/embeds/EightBallEmbedBuilder.ts
@@ -0,0 +1,34 @@
+import { EmbedBuilder } from 'zumito-framework/discord';
+import { TranslationManager, ServiceContainer, EmojiFallback } from "zumito-framework";
+import { config } from "../../../config/index.js";
+
+export class EightBallEmbedBuilder {
+    
+    emojiFallback: EmojiFallback;
+    translator: TranslationManager;
+
+    constructor() {
+        this.emojiFallback = ServiceContainer.getService(EmojiFallback) as EmojiFallback;
+        this.translator = ServiceContainer.getService(TranslationManager);
+    }
+
+    getEmbed({ question, answer, locale }: any) {
+
+        const description = [
+
+            `> ${  this.emojiFallback.getEmoji('', '🤔')  } ${  this.translator.get('eightball.question', locale)}`,
+            `\n> \`\`\`${  question  }\`\`\``,
+            '\n> ',
+            `${  this.emojiFallback.getEmoji('', '🎱')  }${this.translator.get('eightball.answer', locale)}`,
+            `\n> \`\`\`${  answer  }\`\`\``
+        ];
+      
+        const exampleEmbed = new EmbedBuilder()
+        
+            .setThumbnail(config.images.eightBall)
+            .setDescription(description.join(''))
+            .setColor(config.colors.default);
+	
+        return exampleEmbed;
+    }
+}
