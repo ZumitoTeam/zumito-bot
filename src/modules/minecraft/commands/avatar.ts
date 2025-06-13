a2283b6a8a36022fb3206a0fc731b1d435db48ee
Fernando Merino <fernandomema@gmail.com>
2025-06-13 12:29:45 +0200

    feat(minecraft): add mcavatar command

---
+++ src/modules/minecraft/commands/avatar.ts
@@ -0,0 +1,29 @@
+import { EmbedBuilder } from "zumito-framework/discord";
+import { Command, CommandArgDefinition, CommandParameters } from "zumito-framework";
+import { config } from "../../../config/index.js";
+
+export class AvatarCommand extends Command {
+    name = "mcavatar";
+    description = "Shows the Minecraft avatar of a player";
+    categories = ["minecraft"];
+    args: CommandArgDefinition[] = [{
+        name: "username",
+        type: "string",
+        optional: false,
+    }];
+    botPermissions = ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"];
+
+    async execute({ message, interaction, args, trans }: CommandParameters): Promise<void> {
+        const username = args.get("username");
+        if (!username) {
+            (message || interaction)?.reply({ content: trans('error'), allowedMentions: { repliedUser: false } });
+            return;
+        }
+        const embed = new EmbedBuilder()
+            .setTitle(trans('title', { user: username }))
+            .setImage(`https://mc-heads.net/avatar/${encodeURIComponent(username)}`)
+            .setColor(config.colors.default);
+
+        (message || interaction)?.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    }
+}
