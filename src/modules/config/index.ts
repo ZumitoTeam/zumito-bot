2ece01b7ec4a8d2384b6272d82a68a6b4ca3627c
fernandomema <fernandomema@gmail.com>
2025-07-24 12:58:27 +0200

    refactor(config): streamline dashboard item registration in ConfigModule

--- src/modules/config/index.ts
+++ src/modules/config/index.ts
@@ -8,34 +8,19 @@ export class ConfigModule extends Module {
     }
 
     async initialize(): Promise<void> {
-        super.initialize();
-        
+        await super.initialize();
         this.registerDashboardItem();
     }
 
     registerDashboardItem() {
         const navigationService = ServiceContainer.getService(UserPanelNavigationService);
-        navigationService.registerItem({
-            id: 'lang',
-            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 text-discord-white/60 group-hover:text-white icon icon-tabler icons-tabler-outline icon-tabler-language"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5h7" /><path d="M9 3v2c0 4.418 -2.239 8 -5 8" /><path d="M5 9c0 2.144 2.952 3.908 6.7 4" /><path d="M12 20l4 -9l4 9" /><path d="M19.1 18h-6.2" /></svg>`,
-            label: 'Idioma',
-            url: '/panel/:guildId/lang',
-            order: 10,
-            category: 'config',
-            sidebar: {
-                showDropdown: false,
-                sections: [
-                    {
-                        label: 'Configuración',
-                        items: [
-                            {
-                                label: 'Idioma',
-                                url: '/panel/:guildId/lang',
-                            }
-                        ]
-                    }
-                ]
+        navigationService.registerSubItems("dashboard", "General", [
+            {
+                id: "lang",
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 text-discord-white/60 group-hover:text-white icon icon-tabler icons-tabler-outline icon-tabler-language"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5h7" /><path d="M9 3v2c0 4.418 -2.239 8 -5 8" /><path d="M5 9c0 2.144 2.952 3.908 6.7 4" /><path d="M12 20l4 -9l4 9" /><path d="M19.1 18h-6.2" /></svg>`,
+                label: "Idioma",
+                url: "/panel/:guildId/lang",
             },
-        });
+        ]);
     }
 }
