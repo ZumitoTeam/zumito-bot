586e424ead89d71d8960b5c0a4da4c77309da25f
fernandomema <fernandomema@gmail.com>
2025-07-29 18:10:19 +0200

    feat(config): add statusOptions to launcher configuration

--- zumito.config.ts
+++ zumito.config.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import type { LauncherConfig } from 'zumito-framework';
+import { statusOptions } from 'src/config/StatusOptions';
 
 const __dirname = process.cwd();
 
 export const config: LauncherConfig = {
+    statusOptions: statusOptions,
     bundles: [{
         path: path.join(__dirname, "node_modules", "@zumito-team", "admin-module"),
     }, {
