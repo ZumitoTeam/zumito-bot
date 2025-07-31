81afcb048480e39a5d5628bf35d7219744f8644c
fernandomema <fernandomema@gmail.com>
2025-07-31 19:06:09 +0200

    feat(config): update module versions and enhance user panel color configuration

--- zumito.config.ts
+++ zumito.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
-import type { LauncherConfig } from 'zumito-framework';
+import { ServiceContainer, type LauncherConfig } from 'zumito-framework';
+import { UserPanelColorsService } from '@zumito-team/user-panel-module/services/UserPanelColorsService.js';
 import { statusOptions } from 'src/config/StatusOptions';
 
 const __dirname = process.cwd();
@@ -11,5 +12,24 @@ export const config: LauncherConfig = {
     }, {
         path: path.join(__dirname, "node_modules", "@zumito-team", "user-panel-module"),
     }],
+    callbacks: {
+        load: (zumito) => {
+            const userPanelColorsService = ServiceContainer.getService(UserPanelColorsService);
+            userPanelColorsService.setColors({
+                primary: "#e11d48",
+                accent: "#f472b6",
+                success: "#43B581",
+                warning: "#FEE75C",
+                danger: "#ED4245",
+                foreground: "#23272A",
+                dark: {
+                    100: "#E3E5E8",
+                    200: "#F6F6F7",
+                    300: "#FFFFFF",
+                    400: "#F6F6F7"
+                }
+            });
+        }
+    },
 };
 
