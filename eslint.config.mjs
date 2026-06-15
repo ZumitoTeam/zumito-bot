8013e933558c953ab084626c76316da01cbc07ee
WilliamAcosta1 <williamacosta00814@gmail.com>
2026-06-15 15:07:46 +0200

    fix: revert eslint.config.mjs to exactly match main branch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,46 +16,21 @@ export default [
         },
 
         "files": [
+            '**/*.js',
             '**/*.ts',
         ],
 
         "rules": {
+            "no-unused-vars": "error",
             "no-undef": "error",
             "indent": ["error", 4],
 
             "no-multiple-empty-lines": ["error", {
                 max: 1,
             }],
-            "@typescript-eslint/no-unused-vars": ["error", {
-                "args": "none",
-                "varsIgnorePattern": "^_",
-                "caughtErrorsIgnorePattern": "^_"
-            }],
+            "@typescript-eslint/no-unused-vars": "error",
             "@typescript-eslint/no-explicit-any": "warn",
             "@typescript-eslint/no-unused-expressions": "error",
-            "prefer-const": "error",
-            "brace-style": ["error", "1tbs"],
-            "max-depth": ["warn", 4],
-            "object-curly-spacing": ["error", "always"],
-            "array-bracket-spacing": ["error", "never"],
-            "camelcase": ["error", { "properties": "always" }],
-            "prefer-template": "warn",
-            "spaced-comment": ["error", "always", { "markers": ["/"] }]
-        },
-    },
-    {
-        "files": [
-            '**/*.js',
-        ],
-
-        "rules": {
-            "no-undef": "error",
-            "indent": ["error", 4],
-
-            "no-multiple-empty-lines": ["error", {
-                max: 1,
-            }],
-            "no-unused-vars": "error",
             "prefer-const": "error",
             "brace-style": ["error", "1tbs"],
             "max-depth": ["warn", 4],
