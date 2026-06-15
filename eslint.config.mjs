22cddfaaade386e73059e9ec13510f5e6e7c4cf4
WilliamAcosta1 <williamacosta00814@gmail.com>
2026-06-15 03:08:24 +0200

    fix: address PR review comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,12 @@ export default [
             "no-multiple-empty-lines": ["error", {
                 max: 1,
             }],
-            "@typescript-eslint/no-unused-vars": "error",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": ["error", {
+                "argsIgnorePattern": "^_",
+                "varsIgnorePattern": "^_",
+                "caughtErrorsIgnorePattern": "^_"
+            }],
             "@typescript-eslint/no-explicit-any": "warn",
             "@typescript-eslint/no-unused-expressions": "error",
             "prefer-const": "error",
